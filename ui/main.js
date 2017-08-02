d3bf87236f09380ffcba8b2390a144f8ad4fa24b
nitinnjain <njain071@gmail.com>
2017-08-02 23:16:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,6 @@ console.log('Loaded!');
  var element = document.getElementById('main-text');
  
  element.innerHTML = 'New Value by text';
+ 
+ var img = document.getElementById('madi');
+ img.style.margin-left = '100px';
