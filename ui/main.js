1be785ad83a8a05f0bc4f7955f9790e27d56a7f4
nitinnjain <njain071@gmail.com>
2017-08-04 19:30:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ button.onclick = function() {
             if(request.status === 200) {
                 var counter = responseText;
                 var span = document.getElementById('count');
-                span.HTMLcontent = counter.toString();
+                span.innerHTML = counter.toString();
             }
         }
         //Not done yet
