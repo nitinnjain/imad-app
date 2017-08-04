b3974fdb023bab2e73ec3e021307b10c97200bda
nitinnjain <njain071@gmail.com>
2017-08-04 20:23:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,40 +1,54 @@
-var button = document.getElementById('counter');
+// var button = document.getElementById('counter');
 
-button.onclick = function() {
+// button.onclick = function() {
     
-    //Create a request object
-    var request = new XMLHttpRequest();
+//     //Create a request object
+//     var request = new XMLHttpRequest();
     
-    //Capture response and store it in a variable
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            //Take some action
-            if(request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.HTMLcontent = counter.toString();
-            }
+//     //Capture response and store it in a variable
+//     request.onreadystatechange = function() {
+//         if(request.readyState === XMLHttpRequest.DONE) {
+//             //Take some action
+//             if(request.status === 200) {
+//                 var counter = request.responseText;
+//                 var span = document.getElementById('count');
+//                 span.HTMLcontent = counter.toString();
+//             }
+//         }
+//         //Not done yet
+//     };
+    
+//     //Make the request
+//     request.open('GET', 'http://njain071.imad.hasura-app.io/counter', true);
+//     request.send(null);
+// };
+
+// var nameInput = document.getElementById("name");
+// var submit = document.getElementById("submit_btn");
+
+// submit.onclick = function () {
+//     alert('working...');
+//     var names = ['name 1', 'name 2', 'name 3', 'name 4'];
+//     var list ='';
+    
+//     for(var i = 0; i < names.length; i ++) {
+//         list += '<li>' + names[i] + '</li>';
+//     }
+    
+//     var ul = document.getElementById("nameslist");
+//     ul.innerHTMLlist;
+// };
+
+$(document).ready(function () {
+    $('#submit_btn').click(function () {
+        var names = ['name 1', 'name 2', 'name 3', 'name 4'];
+        var list ='';
+        
+        for(var i = 0; i < names.length; i ++) {
+            list += '<li>' + names[i] + '</li>';
         }
-        //Not done yet
-    };
-    
-    //Make the request
-    request.open('GET', 'http://njain071.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
-var nameInput = document.getElementById("name");
-var submit = document.getElementById("submit_btn");
-
-submit.onclick = function () {
-    alert('working...');
-    var names = ['name 1', 'name 2', 'name 3', 'name 4'];
-    var list ='';
-    
-    for(var i = 0; i < names.length; i ++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    
-    var ul = document.getElementById("nameslist");
-    ul.innerHTMLlist;
-};
+        
+        var ul = document.getElementById("nameslist");
+        ul.innerHTML = list;
+    });
+});
