547f8d146e3ce8ca16344633c47dd3bef1b9d651
nitinnjain <njain071@gmail.com>
2017-08-11 12:23:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,13 @@ app.get('/counter', function(req, res) {
     res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function (req, res) {
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));    
+});
+
 app.get('/:articleName', function (req, res) { // this is the feature of express framework
     // This is the object of articles names which we will pass in url bar
     var articleName = req.params.articleName;
@@ -89,13 +96,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names = [];
-app.get('/submit-name/:name', function (req, res) {
-    var name = req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));    
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
