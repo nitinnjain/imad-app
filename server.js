da2ecec991a06aaf66074e77dfed37aae7bc3890
nitinnjain <njain071@gmail.com>
2017-08-12 21:49:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = requirre('pg').Pool;
+
+var config = {
+    user: 'njain071',
+    database: 'njain071',
+    host: 'db.imad.hasura.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -66,8 +75,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+    pool.query('SELECT * FROM test', function (err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        }
+        else {
+            res.send(JSON.stringify(result));
+        }
+    })
+});
+
 var counter = 0;
-app.get('/counter', function(req, res) {
+app.get('/counter', function (req, res) {
     counter = counter +1;
     res.send(counter.toString());
 });
