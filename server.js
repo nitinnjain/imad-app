1e88f8511af5d4a5a923b2f2a12a301db476fa52
nitinnjain <njain071@gmail.com>
2017-08-18 13:42:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/submit-name', function (req, res) {
 app.get('/articles/:articleName', function (req, res) { // this is the feature of express framework
     // This is the object of articles names which we will pass in url bar
     
-    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err, result) {
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err, result) {
         if(err) {
         res.status(500).send(err.toString());
         }
