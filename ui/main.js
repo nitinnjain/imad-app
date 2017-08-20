753b74935496cdea75cae240f8a1e01003053e5b
nitinnjain <njain071@gmail.com>
2017-08-20 20:57:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,34 +1,33 @@
-$(document).ready(function () {
+var submit = document.getElementId('submit_btn');
+
+submit.onclick = function () {
     
-    $('#submit_btn').click(function () {
-        
-        //Create a request object
-        var request = new XMLHttpRequest();
+    //Create a request object
+    var request = new XMLHttpRequest();
     
-        //Capture response and store it in a variable
-        request.onreadystatechange = function() {
-            if(request.readyState === XMLHttpRequest.DONE) {
-                //Take some action
-                if(request.status == 200) {
-                    alert('user loggedin successfully');
-                }
-                else if(request.status == 403) {
-                    alert('username/password incorrect');
-                }
-                else if(request.status == 500) {
-                    alert('Something unexpected happened');
-                }
+    //Capture response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status == 200) {
+                alert('user loggedin successfully');
             }
-            //Not done yet
-        };
-        
-        var username = $('#username').val();
-        var password = $('#password').val();
-        console.log(username);
-        console.log(password);
-        //Make the request
-        request.open('POST', 'http://njain071.imad.hasura-app.io/login', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));
-    });
-});
+            else if(request.status == 403) {
+                alert('username/password incorrect');
+            }
+            else if(request.status == 500) {
+                alert('Something unexpected happened');
+            }
+        }
+        //Not done yet
+    };
+    
+    var username = document.getElementById('username');
+    var password = document.getElementById('password');
+    console.log(username);
+    console.log(password);
+    //Make the request
+    request.open('POST', 'http://njain071.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
