36355708113867b73c2b4bca9e123c32a12f31ad
nitinnjain <njain071@gmail.com>
2017-08-20 21:03:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,8 +22,8 @@ submit.onclick = function () {
         //Not done yet
     };
     
-    var username = document.getElementById('username');
-    var password = document.getElementById('password');
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
     //Make the request
