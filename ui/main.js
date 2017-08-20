c27e0cdef77127cd083b1ff9dd9448afbb28e142
nitinnjain <njain071@gmail.com>
2017-08-20 20:25:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,3 @@
-var button = document.getElementById('counter');
-
-button.onclick = function() {
-    
-    //Create a request object
-    var request = new XMLHttpRequest();
-    
-    //Capture response and store it in a variable
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            //Take some action
-            if(request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        //Not done yet
-    };
-    
-    //Make the request
-    request.open('GET', 'http://njain071.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
 $(document).ready(function () {
     
     $('#submit_btn').click(function () {
@@ -35,24 +10,25 @@ $(document).ready(function () {
             if(request.readyState === XMLHttpRequest.DONE) {
                 //Take some action
                 if(request.status === 200) {
-                    var names = request.responseText;
-                    names = JSON.parse(names);
-                    var list ='';
-            
-                    for(var i = 0; i < names.length; i ++) {
-                    list += '<li>' + names[i] + '</li>';
-                    }
-            
-                    var ul = document.getElementById("nameslist");
-                    ul.innerHTML = list;
+                    alert('user loggedin successfully');
+                }
+                else if(request.status === 403) {
+                    alert('username/password incorrect');
+                }
+                else if(request.status === 500) {
+                    alert('Something unexpected happened');
                 }
             }
             //Not done yet
         };
         
-        var name = $('#name').val();
+        var username = $('#username').val();
+        var password = $('#password').val();
+        console.log(username);
+        console.log(password);
         //Make the request
-        request.open('GET', 'http://njain071.imad.hasura-app.io/submit-name?name=' + name, true);
-        request.send(null);
+        request.open('POST', 'http://njain071.imad.hasura-app.io/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify{username: username, password: password});
     });
 });
