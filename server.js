e071b82fef2de58157b4ae39797db61d6fdccd7f
nitinnjain <njain071@gmail.com>
2017-08-21 20:15:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.get('/logout', function(req, res) {
     res.send('Logged out');
 });
 
-app.get('/getarticles', function () {
+app.get('/get-articles', function () {
     pool.query('SELECT * FROM article', function (err, result) {
         if(err) {
             res.status(500).send(err.toString());
