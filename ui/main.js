e22cdf9e624405e4cda474f30c9a37d294eac760
nitinnjain <njain071@gmail.com>
2017-08-21 23:08:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,42 +25,6 @@
 //     register_btn.vlaue = 'Registering...';
 // };
 
-// var login_btn = document.getElementById('login_btn');
-        
-// login_btn.onclick = function () {
-    
-//     //Create a request object
-//     var request = new XMLHttpRequest();
-    
-//     //Capture response and store it in a variable
-//     request.onreadystatechange = function() {
-//         if(request.readyState === XMLHttpRequest.DONE) {
-//             // Take some action
-//             if (request.status === 200) {
-//                 submit.value = 'Sucess!';
-//             } else if (request.status === 403) {
-//                 submit.value = 'Invalid credentials. Try again?';
-//             } else if (request.status === 500) {
-//                 alert('Something went wrong on the server');
-//                 submit.value = 'Login';
-//             } else {
-//                 alert('Something went wrong on the server');
-//                 submit.value = 'Login';
-//             }
-//             loadLogin();
-//         }
-//         //Not done yet
-//     };
-    
-//     //Make the request
-//     var username = document.getElementById('username').value;
-//     var password = document.getElementById('password').value;
-//     request.open('POST', 'http://njain071.imad.hasura-app.io/login', true);
-//     request.setRequestHeader('Content-Type', 'application/json');
-//     request.send(JSON.stringify({username: username, password: password}));
-//     login_btn.value = 'Logging in...';
-// };
-
 show_articles();
 check_login();
 
