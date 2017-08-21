5223b2b4df41b46db9098aeef9904253f4de05ee
nitinnjain <njain071@gmail.com>
2017-08-21 22:05:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -102,7 +102,7 @@ function show_articles() {
                 for(var i = 0; i < articleData.length; i++) {
                     content += `<li>
                                 <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
-                                (${articleData[i].date})
+                                (${articleData[i].date.split('T')})
                                 </li>
                     `;
                 }
