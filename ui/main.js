a7ab5593a664b869cecad6610f6d314b8010b13f
nitinnjain <njain071@gmail.com>
2017-08-21 10:43:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,6 +28,6 @@ submit.onclick = function () {
     console.log(username);
     console.log(password);
     request.open('POST', 'http://njain071.imad.hasura-app.io/login', true);
-    request.setRequestHeader('Content-Type', 'application/json');
+    request.setRequestHeader('Content-Type', 'application/json')
     request.send(JSON.stringify({username: username, password: password}));
 };
