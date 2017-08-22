4451c368513e0064fb1b82e648c62a5f52662a8c
nitinnjain <njain071@gmail.com>
2017-08-22 13:21:26 +0530

    [imad-console] Updates ui/articles.js

--- ui/articles.js
+++ ui/articles.js
@@ -1,4 +1,4 @@
-var article_name = window.location.pathname.split('/')[3];
+var article_name = window.location.pathname.split('/')[1];
 alert(article_name)
 //firstly check if the user is logged in or not
 check_login();
