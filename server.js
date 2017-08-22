9999ab358fc4da35a212dd1fc63dee93ab28d3e3
nitinnjain <njain071@gmail.com>
2017-08-22 19:27:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,6 +147,35 @@ app.get('/logout', function(req, res) {
     res.send('Logged out');
 });
 
+app.get('/submit-comment/:articleName', function (req, res) {
+    if(req.session && req.session.auth && req.session.auth.userId) {
+        pool.query('SELECT * FROM "user" WHERE title = $1', [req.params.articleName], function (err, result) {
+            if(err) {
+                res.status(500).send(err.toString());
+            }
+            else {
+                if(result.rows.length === 0) {
+                    res.status(400).send('Article not found');
+                }
+                else {
+                    var articleId = result.rows[0].id;
+                    pool.query('INSERT INTO comment (article_id, user_id, comment) VALUE($1, $2, $3)', [article_id, erq.session.auth.userId, req.body.comment], function (err, result) {
+                        if(err) {
+                            res.status(500).send(err.toString());
+                        }
+                        else {
+                            res.status(200).send('Comment submitted successfully.');
+                        }
+                    });
+                }
+            }
+        });
+    }
+    else {
+        res.status(403).send('only loggedin users can coomment!');
+    }
+});
+
 app.get('/get-articles', function (req, res) {
     pool.query('SELECT * FROM article ORDER BY date DESC', function (err, result) {
         if(err) {
